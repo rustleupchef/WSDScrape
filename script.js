6531c4e54a7e32179d9a48f0e45fa872759d2258
rustleupchef <rustle.up.chef@gmail.com>
2026-05-21 21:39:38 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,96 @@
+import { JSDOM } from "jsdom";
+import TurndownService from "turndown";
+import { writeFile } from "node:fs/promises";
+import { YoutubeTranscript } from "youtube-transcript";
+import yts from "yt-search";
+import path from "path";
+
+const turndownservice = new TurndownService();
+
+async function grabWOWIAnalysis() {
+    const baseUrl = "https://www.worldofwordsinstitute.com";
+    const url = `${baseUrl}/blog/category/Topic+Analyses`;
+
+    const response = await fetch(url);
+    const html = await response.text();
+
+    const doc = new JSDOM(html).window.document;
+    const blogItemSummary = doc.querySelectorAll(".blog-title");
+
+    const links = [];
+    blogItemSummary.forEach(element => {
+        links.push(element.querySelector("a").href);
+    });
+
+    console.log(`Found ${links.length} link(s). Starting text extraction...\n`);
+    for (const link of links) {
+        console.log(`🔗 Processing: ${link}`);
+
+        const pageResponse = await fetch(baseUrl + link);
+        const pageHtml = await pageResponse.text();
+
+        const dom = new JSDOM(html).window.document;
+        let content = "";
+
+        const textBoxes = dom.querySelectorAll(".sqs-text-block-container");
+        for (let i = 0; i < textBoxes.length; i++) {
+            const html = textBoxes[i].innerHTML;
+            const markdown = turndownservice.turndown(html);
+            content += markdown + "\n";
+        }
+        
+        const filePath = "output/" + crypto.randomUUID() + ".md";
+        await writeFile(filePath, content, "utf8");
+        console.log(`✅ Saved transcript to: ${filePath}\n`);
+    }
+    console.log('🎉 Scraping task completed!');
+}
+
+async function scrapeDebateTranscripts() {
+    const SEARCH_QUERY = 'World Schools Debating Championship WSDC finals';
+    const MAX_VIDEOS = 20;
+
+    console.log(`🔍 Searching YouTube for: "${SEARCH_QUERY}"...`);
+
+    const searchResults = await yts(SEARCH_QUERY);
+    const videos = searchResults.videos.slice(0, MAX_VIDEOS);
+
+    if (videos.length === 0) {
+        console.log('No videos found.');
+        return;
+    }
+
+    console.log(` Found ${videos.length} videos. Starting transcript extraction...\n`);
+
+    for (const video of videos) {
+        console.log(`🎬 Processing: "${video.title}"`);
+        console.log(`🔗 Link: ${video.url}`);
+
+        try {
+            const transcriptItems = await YoutubeTranscript.fetchTranscript(video.videoId);
+    
+            const fullText = transcriptItems
+                .map(item => item.text)
+                .join(' ')
+                .replace(/&#39;/g, "'")
+                .replace(/&quot;/g, '"');
+    
+            const filePath = path.join("output/", `${crypto.randomUUID()}.md`);
+    
+            await writeFile(filePath, fullText, 'utf-8');
+            console.log(`✅ Saved transcript to: ${filePath}\n`);
+        } catch (error) {
+            console.log(`❌ Failed to save transcript because: ${error}\n`);
+        }
+
+    }
+
+    console.log('🎉 Scraping task completed!');
+}
+
+async function main() {
+    await grabWOWIAnalysis();
+    await scrapeDebateTranscripts();
+}
+
+main();
