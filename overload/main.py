637ccfe7a54bce2b17cbcdec11940db64d8e9ee8
rustleupchef <rustle.up.chef@gmail.com>
2026-05-25 19:07:54 -0500

    added way to manage overload of token counts in the output folder

--- overload/main.py
+++ overload/main.py
@@ -1,5 +1,24 @@
 import os
+import sys
+import subprocess
 import tiktoken
+
+def scrape(directory_path, count):
+    subprocess.run(["node", "script.js", str(count)], cwd=directory_path, stdout=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+
+def is_greater(desired: int, counted: tuple):
+        return desired > counted[0] and desired > counted[-1]
+
+def avg_diff(desired: int, counted: tuple):
+    return (counted[0]/desired + counted[-1]/desired)/2
+
+def delete_all_files(directory_path, exceptions = [".gitignore"]):
+    for filename in os.listdir(directory_path):
+        if filename in exceptions: continue
+        os.remove(os.path.join(directory_path, filename))
+
+def count_files(directory_path, exceptions = [".gitignore"]):
+    return len([file for file in os.listdir(directory_path) if file not in exceptions])
 
 def count_tokens_in_directory(directory_path, model="gpt-4o"):
     encoding = tiktoken.encoding_for_model(model)
@@ -20,15 +39,64 @@ def count_tokens_in_directory(directory_path, model="gpt-4o"):
             except Exception as e:
                 print(f"Could not read {file}: {e}")
                 
-    return total_tokens, letters/4
+    return total_tokens - 4, letters/4 - 3 # accounting for the .gitignore
 
-def main():
+def main(arguments = []):
     parent_dir = os.path.dirname(os.getcwd())
     output_dir = os.path.join(parent_dir, "output")
 
     print(f"gpt_count, estimate : {count_tokens_in_directory(output_dir)}")
-
     
+    if len(arguments) == 0: return
+    goal_score = int(arguments[0])
+    delete_all_files(output_dir)
+
+    print(f"\n\n{'| Working to match your token count |'.center(20 + len('| Working to match your token count |'), '=')}\n\n")
+
+    print("Test 1: Checking Lower Bound of Token Count")
+    scrape(parent_dir, 0)
+    lower = count_tokens_in_directory(output_dir)
+    if not is_greater(goal_score, lower):
+        print("❌ Level 2 Overload: your token count is far too small even for WOWI alone")
+        return
+    lower = count_files(output_dir)
+    delete_all_files(output_dir)
+    print("✅ Test 1 passed\n\n")
+
+    print("Test 2: Checking Upper Bound of Token Count")
+    scrape(parent_dir, 1000)
+    upper = count_tokens_in_directory(output_dir)
+    if is_greater(goal_score, upper):
+        print("✅ Test 2 passed")
+        return
+    upper = count_files(output_dir)
+    delete_all_files(output_dir)
+    print("❌ Level 1 Overload: your token count is far too small for all videos\n\n")
+
+    print("Test 3: beginning to truncate...")
+    upper, lower = upper - lower, 0
+
+    while True:
+        middle = (upper + lower)//2
+        print(f"{lower=}\t{upper=}\t{middle=}")
+
+        scrape(parent_dir, middle)
+        point = count_tokens_in_directory(output_dir)
+        print(f"gpt_count, estimate : {point}")
+        diff = avg_diff(goal_score, point)
+        print(f"diff={abs(1 - diff)}")
+
+        if is_greater(goal_score, point):
+            print("=>")
+            if diff >= .95:
+                print("✅ Test 3 passed")
+                break
+            lower = middle
+        else:
+            print("<=")
+            upper = middle
+        delete_all_files(output_dir)
+        print("-" * 20)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
